9cffff9917af4e565fcd740e69262a5232be17aa
cvlong <cvlong@gmail.com>
2016-04-11 17:09:31 -0700

    Working with docstrings

--- wordcount.py
+++ wordcount.py
@@ -1,37 +1,32 @@
-# put your code here.
-
 def wordcount_short(text_file):
+    """Prints the number of times each word appears in a file"""
+
+
     text = open(text_file)
 
-    all_lists = []
     word_count = {}
 
     for line in text:
         line = line.rstrip()
         line = line.split(" ")
-        # print all_lines
-        all_lists.append(line)
-    print all_lists
 
-    #word_count = {"seven": 4, "Kits": 1, "sack": 1}
-    #key = word
-    #value = number of times
-
-        # word_count["seven"] = 1
-        # word_count["seven"] = value + 1
-        # word_count[word] = count
-
-    #1. add each key once to word_count, and assign value to 1
-    for line in all_lists:
         for word in line:
-            word_count[word] = 1
-    return word_count
-  
-    #2. loop through text_file
 
-    #3. return key, value
+            #Adds first instance of each word to the dictionary with value 1
+            if word not in word_count:
+                word_count[word] = 1
 
-print wordcount_short("test.txt")
+            #Increases value count for each additional instance of word
+            elif word in word_count:
+                word_count[word] += 1
+
+    for word, count in word_count.items():
+        print word, count
+
+    text.close()
+
+
+wordcount_short("test.txt")
 
 
 
